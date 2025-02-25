4d845f555c8f0afbf818eb3aa2ce859d995685f2
atakan-m <atakanmertoglu@gmail.com>
2025-02-25 13:49:51 +0100

    ra

--- LAB 2/instructions.cpp
+++ LAB 2/instructions.cpp
@@ -1,6 +1,30 @@
 #include "instruction.h"
 #include <iostream>
 
+// Define AddInstruction
+AddInstruction::AddInstruction(int d, int s1, int s2) : dest(d), src1(s1), src2(s2) {}
+
+void AddInstruction::disassemble() {
+    std::cout << "add $" << dest << ", $" << src1 << ", $" << src2 << std::endl;
+}
+
+int AddInstruction::execute(Registers *regs) {
+    regs->setRegister(dest, regs->getRegister(src1) + regs->getRegister(src2));
+    return regs->getPC() + 1;
+}
+
+// Define SubInstruction
+SubInstruction::SubInstruction(int d, int s1, int s2) : dest(d), src1(s1), src2(s2) {}
+
+void SubInstruction::disassemble() {
+    std::cout << "sub $" << dest << ", $" << src1 << ", $" << src2 << std::endl;
+}
+
+int SubInstruction::execute(Registers *regs) {
+    regs->setRegister(dest, regs->getRegister(src1) - regs->getRegister(src2));
+    return regs->getPC() + 1;
+}
+
 // Define OriInstruction
 OriInstruction::OriInstruction(int d, int s, int i) : dest(d), src(s), imm(i) {}
 
