54409bae75a33bd54c0a7805a580cdcffd750b8c
Parama <paramatodos@gmail.com>
2025-02-25 18:50:46 +0100

    progress assignment 2.2 and 2.3

--- paramas_cookery/LAB2/ISS/assignment2.2/registers.h
+++ paramas_cookery/LAB2/ISS/assignment2.2/registers.h
@@ -1,8 +1,31 @@
 #ifndef _REGISTERS_H_
 #define _REGISTERS_H_
-
+#include <iostream>
 class Registers
 {
+public:
+	class Register{
+		public:
+			int regNum;
+			int value;
+			Register(int a = 0, int b = 0) : regNum(a), value(b) {}
+			};
+	Register reg[31];
+
+	void setRegister(int a, int b);
+	int getRegister(int a);
+
+	class ProgramCounter{
+		public: 
+			int counter;
+	};
+
+	ProgramCounter PC;
+	void setPC(int a);
+	int getPC();
+	void print();
+
+
 };
 
 #endif	/* _REGISTERS_H_ */
