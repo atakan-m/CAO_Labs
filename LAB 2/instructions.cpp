9f3c5ad1044804457eab06bd56c1447c3113ca2d
atakan-m <atakanmertoglu@gmail.com>
2025-02-25 13:48:40 +0100

    Update instructions.cpp

--- LAB 2/instructions.cpp
+++ LAB 2/instructions.cpp
@@ -1,26 +1,25 @@
 #include "instruction.h"
 #include <iostream>
 
-// Define AddInstruction
-AddInstruction::AddInstruction(int d, int s1, int s2) : dest(d), src1(s1), src2(s2) {}
+// Define OriInstruction
+OriInstruction::OriInstruction(int d, int s, int i) : dest(d), src(s), imm(i) {}
 
-void AddInstruction::disassemble() {
-    std::cout << "add $" << dest << ", $" << src1 << ", $" << src2 << std::endl;
+void OriInstruction::disassemble() {
+    std::cout << "ori $" << dest << ", $" << src << ", " << imm << std::endl;
 }
 
-int AddInstruction::execute(Registers *regs) {
-    regs->setRegister(dest, regs->getRegister(src1) + regs->getRegister(src2));
+int OriInstruction::execute(Registers *regs) {
+    regs->setRegister(dest, regs->getRegister(src) | imm);
     return regs->getPC() + 1;
 }
 
-// Define SubInstruction
-SubInstruction::SubInstruction(int d, int s1, int s2) : dest(d), src1(s1), src2(s2) {}
+// Define BrneInstruction
+BrneInstruction::BrneInstruction(int s1, int s2, int off) : src1(s1), src2(s2), offset(off) {}
 
-void SubInstruction::disassemble() {
-    std::cout << "sub $" << dest << ", $" << src1 << ", $" << src2 << std::endl;
+void BrneInstruction::disassemble() {
+    std::cout << "brne $" << src1 << ", $" << src2 << ", " << offset << std::endl;
 }
 
-int SubInstruction::execute(Registers *regs) {
-    regs->setRegister(dest, regs->getRegister(src1) - regs->getRegister(src2));
-    return regs->getPC() + 1;
+int BrneInstruction::execute(Registers *regs) {
+    return (regs->getRegister(src1) != regs->getRegister(src2)) ? regs->getPC() + offset : regs->getPC() + 1;
 }
